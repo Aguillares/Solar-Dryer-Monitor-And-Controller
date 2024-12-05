e3ef196a658b379559ecff45c707db2b88f27759
Antonio Aguillares <aguillaresantonio@gmail.com>
2024-12-05 15:50:06 -0600

    added

--- VirtualButton.py
+++ VirtualButton.py
@@ -2,6 +2,9 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
 try:
     GPIO.output(16,True)
     while(True):
