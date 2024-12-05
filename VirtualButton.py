e834e5090a98fe25068d176983bd3974c9534625
Antonio Aguillares <aguillaresantonio@gmail.com>
2024-12-05 15:22:28 -0600

    Added

--- VirtualButton.py
+++ VirtualButton.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT)
 try:
-    GPIO.output(12,True)
+    GPIO.output(16,True)
     while(True):
+        print("High")
         pass
 except KeyboardInterrupt:
     pass
 finally:
+    print("Low")
     GPIO.cleanup()
