16654c74a94e2178b461dd30277db2140c2ff5c5
Antonio Aguillares <aguillaresantonio@gmail.com>
2024-12-08 17:39:32 -0600

    added

--- adafruitadc.py
+++ adafruitadc.py
@@ -19,16 +19,17 @@ c2 = 2.081779068e-04
 c3 = 1.250512199e-07
 
 
-def calculate_temperature(adc_value):
+def calculate_temperature():
+    global thermistor
     # Calculate the thermistor resistance   
-    voltage = adc_value * (3.3/ 255.0)  # Assuming 3.3V reference
-    R2 = R1 * (3.3/ voltage - 1.0)
+    voltage = thermistor.value  # Assuming 3.3V reference
+    R2 = R1 * (1/ voltage - 1.0)
 
     # Calculate temperature using Steinhart-Hart equation
     logR2 = math.log(R2)
     temperature_kelvin = 1.0 / (c1 + c2 * logR2 + c3 * logR2**3)
     temperature_celsius = temperature_kelvin - 273.15
-    print(f"ADC Value: {adc_value}")
+   
     print(f"Voltage: {voltage}")
     print(f"Thermistor Resistance: {R2}")
     print(f"logR2: {logR2}")
@@ -38,7 +39,8 @@ if __name__ == '__main__':
     try:
         thermistor = MCP3008(0)
         while True:
-            print(f"Temperature: {thermistor.value} ºC")
+            temp=calculate_temperature()
+            print(f"Temperature: {temp} ºC")
             time.sleep(0.5)
 
     except KeyboardInterrupt:
