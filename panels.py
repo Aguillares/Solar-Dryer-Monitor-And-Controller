855f03ad830d08f8a9168609523579488bf5ecca
Antonio Hernandez Aguillares <aguillaresantonio@gmail.com>
2025-02-23 17:06:27 -0700

    altered

--- panels.py
+++ panels.py
@@ -43,6 +43,7 @@ class ScrollbarFrame(ttk.Frame):
                                          relwidth = 1,
                                          anchor = 'nw')
         ttk.Label(self, font = font_, text = title)
+        print('hello')
 
 
 # PanelData holds all the widgets are inside it.
