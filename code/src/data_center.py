99381150b1834f772c4a000dd08e08e37581f8a8
Antonio Hernandez Aguillares <aguillaresantonio@gmail.com>
2026-09-09 15:26:40 -0700

    Correcting the error 'There are no sensors connected

--- code/src/data_center.py
+++ code/src/data_center.py
@@ -259,7 +259,7 @@ class SensorsController():
         """Removes the sensors that are not connected"""
         type_obj=list(self._control_center.items())
         for type_, obj_addr in type_obj:
-            total_num = len(obj_addr[1])
+            total_num = len(obj_addr[0])
             if total_num > 0:
                 print(f"{total_num} " + type_ + ' connected. Addresses: ', end='')
                 for curr_num, addr in enumerate(obj_addr[1]):
